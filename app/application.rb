1f4cfb540ec24ce725116e8e16a4d98e59a25dda
Srikalyan Megati <kalmegati@gmail.com>
2019-11-26 19:21:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,14 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
+    hour = Time.now
+    
+    if hour >= 12
+      
+    else
+      
+    end
+    
     resp.finish
   end
   
